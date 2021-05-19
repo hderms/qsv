8d7ff4f551ac42a1c99fa99efb5245bf13e73dc5
Dermot Haughey <hderms@gmail.com>
2021-05-19 08:37:20 -0500

    cargo fmt

--- src/db/functions.rs
+++ src/db/functions.rs
@@ -1,7 +1,7 @@
 use rusqlite::functions::{Aggregate, Context, FunctionFlags};
-use rusqlite::{Connection, Result};
-use stats::{OnlineStats};
 use rusqlite::types::ValueRef;
+use rusqlite::{Connection, Result};
+use stats::OnlineStats;
 
 pub(crate) fn add_udfs(connection: &Connection) -> Result<()> {
     connection.create_scalar_function(
@@ -22,12 +22,7 @@ pub(crate) fn add_udfs(connection: &Connection) -> Result<()> {
         FunctionFlags::SQLITE_DETERMINISTIC,
         Stddev,
     )?;
-    connection.create_aggregate_function(
-        "mean",
-        1,
-        FunctionFlags::SQLITE_DETERMINISTIC,
-        Mean,
-    )?;
+    connection.create_aggregate_function("mean", 1, FunctionFlags::SQLITE_DETERMINISTIC, Mean)?;
     Ok(())
 }
 
@@ -59,10 +54,14 @@ impl Aggregate<OnlineStats, Option<f64>> for Stddev {
     fn step(&self, ctx: &mut Context<'_>, stdev: &mut OnlineStats) -> Result<()> {
         let value = ctx.get_raw(0);
         match value {
-            ValueRef::Null => { stdev.add_null() }
-            ValueRef::Integer(i) => { stdev.add(i as f64) }
-            ValueRef::Real(f) => { stdev.add(f) }
-            ValueRef::Text(_) | ValueRef::Blob(_) => { return Result::Err(rusqlite::Error::UserFunctionError("can't take stddev of this value".into())); }
+            ValueRef::Null => stdev.add_null(),
+            ValueRef::Integer(i) => stdev.add(i as f64),
+            ValueRef::Real(f) => stdev.add(f),
+            ValueRef::Text(_) | ValueRef::Blob(_) => {
+                return Result::Err(rusqlite::Error::UserFunctionError(
+                    "can't take stddev of this value".into(),
+                ));
+            }
         }
         Ok(())
     }
@@ -83,10 +82,14 @@ impl Aggregate<OnlineStats, Option<f64>> for Mean {
     fn step(&self, ctx: &mut Context<'_>, mean: &mut OnlineStats) -> Result<()> {
         let value = ctx.get_raw(0);
         match value {
-            ValueRef::Null => { mean.add_null() }
-            ValueRef::Integer(i) => { mean.add(i as f64) }
-            ValueRef::Real(f) => { mean.add(f) }
-            ValueRef::Text(_) | ValueRef::Blob(_) => { return Result::Err(rusqlite::Error::UserFunctionError("can't take mean of this value".into())); }
+            ValueRef::Null => mean.add_null(),
+            ValueRef::Integer(i) => mean.add(i as f64),
+            ValueRef::Real(f) => mean.add(f),
+            ValueRef::Text(_) | ValueRef::Blob(_) => {
+                return Result::Err(rusqlite::Error::UserFunctionError(
+                    "can't take mean of this value".into(),
+                ));
+            }
         }
         Ok(())
     }
