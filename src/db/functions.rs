ddf91a5e8afcbc2da79593aa30e7ec364b542691
Dermot Haughey <hderms@gmail.com>
2021-05-19 09:34:21 -0500

    add mean as UDF (#43)
    * add mean as UDF
    
    * cargo fmt

--- src/db/functions.rs
+++ src/db/functions.rs
@@ -1,4 +1,5 @@
 use rusqlite::functions::{Aggregate, Context, FunctionFlags};
+use rusqlite::types::ValueRef;
 use rusqlite::{Connection, Result};
 use stats::OnlineStats;
 
@@ -21,8 +22,10 @@ pub(crate) fn add_udfs(connection: &Connection) -> Result<()> {
         FunctionFlags::SQLITE_DETERMINISTIC,
         Stddev,
     )?;
+    connection.create_aggregate_function("mean", 1, FunctionFlags::SQLITE_DETERMINISTIC, Mean)?;
     Ok(())
 }
+
 pub(crate) fn calculate_md5(ctx: &Context) -> Result<String> {
     assert_eq!(ctx.len(), 1, "called with unexpected number of arguments");
     let str = ctx.get_raw(0).as_str()?;
@@ -40,6 +43,7 @@ pub(crate) fn calculate_sqrt(ctx: &Context) -> Result<f64> {
         Ok((i64 as f64).sqrt())
     }
 }
+
 pub struct Stddev;
 
 impl Aggregate<OnlineStats, Option<f64>> for Stddev {
@@ -48,13 +52,50 @@ impl Aggregate<OnlineStats, Option<f64>> for Stddev {
     }
 
     fn step(&self, ctx: &mut Context<'_>, stdev: &mut OnlineStats) -> Result<()> {
-        let next = ctx.get::<f64>(0)?;
-        stdev.add(next);
+        let value = ctx.get_raw(0);
+        match value {
+            ValueRef::Null => stdev.add_null(),
+            ValueRef::Integer(i) => stdev.add(i as f64),
+            ValueRef::Real(f) => stdev.add(f),
+            ValueRef::Text(_) | ValueRef::Blob(_) => {
+                return Result::Err(rusqlite::Error::UserFunctionError(
+                    "can't take stddev of this value".into(),
+                ));
+            }
+        }
         Ok(())
     }
 
     fn finalize(&self, _: &mut Context<'_>, numbers: Option<OnlineStats>) -> Result<Option<f64>> {
         let stddev = numbers.map(|n| n.stddev());
         Ok(stddev)
+    }
+}
+
+pub struct Mean;
+
+impl Aggregate<OnlineStats, Option<f64>> for Mean {
+    fn init(&self, _: &mut Context<'_>) -> Result<OnlineStats> {
+        Ok(OnlineStats::new())
+    }
+
+    fn step(&self, ctx: &mut Context<'_>, mean: &mut OnlineStats) -> Result<()> {
+        let value = ctx.get_raw(0);
+        match value {
+            ValueRef::Null => mean.add_null(),
+            ValueRef::Integer(i) => mean.add(i as f64),
+            ValueRef::Real(f) => mean.add(f),
+            ValueRef::Text(_) | ValueRef::Blob(_) => {
+                return Result::Err(rusqlite::Error::UserFunctionError(
+                    "can't take mean of this value".into(),
+                ));
+            }
+        }
+        Ok(())
+    }
+
+    fn finalize(&self, _: &mut Context<'_>, numbers: Option<OnlineStats>) -> Result<Option<f64>> {
+        let mean = numbers.map(|n| n.mean());
+        Ok(mean)
     }
 }
